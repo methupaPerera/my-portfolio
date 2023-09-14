a121709b2622ceb30f7abf53b0bcad1a30639061
Methupa Perera <methupapereradev@gmail.com>
2023-09-14 08:48:17 +0530

    Added particleJS to the background.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-import { Navbar } from "./components";
+import { Navbar, Particle } from "./components";
 import PrimaryLayout from "./layouts/PrimaryLayout";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="text-white _gradient">
+        <div className="text-white relative _gradient">
             <Navbar />
             <Routes>
                 <Route index element={<PrimaryLayout />} />
             </Routes>
+            <div className="absolute">
+                <Particle />
+            </div>
         </div>
     );
 };
---
+++ src/components/Particle.jsx
@@ -0,0 +1,88 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
+const Particle = () => {
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 2,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#7db7ff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.2,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 900,
+                        },
+                        value: 30,
+                    },
+                    opacity: {
+                        value: 0.2,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 5, max: 20 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
+};
+
+export default Particle;
