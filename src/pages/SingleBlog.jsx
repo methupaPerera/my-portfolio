4639bd7f817038765c5d92efb68faef929979845
Methupa Perera <methupapereradev@gmail.com>
2023-09-26 09:12:54 +0530

    Made minor changes.

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import { blogList } from "../data/blogList";
+
 const SingleBlog = () => {
     const { id } = useParams();
     return <div>Single Blog {id}</div>;
