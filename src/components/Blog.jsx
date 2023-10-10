56d5fe4f1dc6046665cb1f98f18d05466e33bd8b
Methupa Perera <methupapereradev@gmail.com>
2023-10-10 21:29:03 +0530

    `Minor changes.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 const Blog = ({ id, heading, image, text, date, index }) => {
     return (
-        <Link to={`/all-blogs/${id}`} className="backdrop-blur-lg z-40">
+        <Link to={`/all-blogs/${id}`} className="backdrop-blur-lg rounded-xl border-[1.5px] border-[#ffffff15] z-40">
             <motion.div
                 initial={{ opacity: 0, translateY: 150 }}
                 whileInView={{ opacity: 1, translateY: 0 }}
                 transition={{ duration: 0.4, delay: index / 10 }}
-                className="flex flex-col gap-5 bg-slate-700 p-4 text-[0.8rem] justify-center rounded-xl bg-opacity-25 border-[1.5px] border-[#ffffff15] cursor-pointer"
+                className="flex flex-col gap-5 bg-slate-700 p-4 text-[0.8rem] justify-center bg-opacity-25 cursor-pointer"
             >
                 <h3 className="text-xl font-medium">{heading}</h3>
                 <img src={image} className="w-full rounded-lg" />
