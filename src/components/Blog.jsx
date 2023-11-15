473169a3bb35831b22984e97159c1895cda734f8
Methupa Perera <methupapereradev@gmail.com>
2023-11-15 09:27:31 +0530

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,7 +8,7 @@ const Blog = ({ id, heading, image, text, date, index }) => {
             initial={{ opacity: 0, translateY: 50 }}
             whileInView={{ opacity: 1, translateY: 0 }}
             transition={{ duration: 0.4, delay: index / 10 }}
-            className="p-4 text-[0.8rem] bg-slate-700 bg-opacity-25 backdrop-blur-lg rounded-xl border-[1.5px] border-[#ffffff15] z-40 cursor-pointer"
+            className="p-6 text-[0.8rem] bg-slate-700 bg-opacity-25 backdrop-blur-lg rounded-xl border-[1.5px] border-[#ffffff15] z-40 cursor-pointer"
         >
             <Link
                 to={`/all-blogs/${id}`}
