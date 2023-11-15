24cb211c20bb63690cfa20b4624fa83cc50ac3dd
Methupa Perera <methupapereradev@gmail.com>
2023-11-15 09:14:50 +0530

    Edited the home page.

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
     return (
         <div id="home" className="px-6 min-h-screen flex items-center">
             <div className="flex">
-                <div className="w-[20px] bg-white rounded-full"></div>
-                <div className="py-2">
+                <div className="w-2 bg-white rounded-full"></div>
+                <div className="py-6">
                     <div className="mb-8 ml-1 hidden md:block">
                         <ProfileLarge />
                     </div>
@@ -20,24 +20,24 @@ const Home = () => {
                                 transition={{ duration: 0.4, delay: 0.1 }}
                                 className="mt-1 mr-2 w-10 h-[8px] bg-white rounded-full"
                             ></motion.span>
-                            Methupa Perera{" "}
+                            Methupa Perera
                         </h1>
 
                         <h2 className="mt-6 -ml-3 flex items-center text-slate-400 font-medium md:text-lg">
                             <motion.span
                                 initial={{ width: 0 }}
                                 whileInView={{ width: 50 }}
-                                transition={{ duration: 0.4, delay: 0.1 }}
+                                transition={{ duration: 0.4, delay: 0.15 }}
                                 className="mr-2 w-10 h-[8px] bg-white rounded-full"
                             ></motion.span>
-                            Making Things Interesting!{" "}
+                            Making Things Interesting !
                         </h2>
 
                         <h3 className="mt-8 -ml-3 flex items-center font-medium text-lg">
                             <motion.span
                                 initial={{ width: 0 }}
                                 whileInView={{ width: 50 }}
-                                transition={{ duration: 0.4, delay: 0.1 }}
+                                transition={{ duration: 0.4, delay: 0.2 }}
                                 className="mr-2 w-10 h-[8px] bg-white rounded-full"
                             ></motion.span>
                             18 Years{" "}
@@ -47,23 +47,11 @@ const Home = () => {
                             <motion.span
                                 initial={{ width: 0 }}
                                 whileInView={{ width: 50 }}
-                                transition={{ duration: 0.4, delay: 0.1 }}
+                                transition={{ duration: 0.4, delay: 0.25 }}
                                 className="mr-2 w-10 h-[8px] bg-white rounded-full"
                             ></motion.span>
                             Bandaragama, Sri Lanka{" "}
                         </h4>
-
-                        <p className="mt-4 -ml-3 flex items-center font-medium text-slate-400">
-                            <motion.span
-                                initial={{ width: 0 }}
-                                whileInView={{ width: 76 }}
-                                transition={{ duration: 0.4, delay: 0.1 }}
-                                className="mr-2 w-10 h-[8px] bg-white rounded-full"
-                            ></motion.span>
-                            Interested in the entire frontend and backend
-                            spectrum and working on ambitious projects with
-                            positive people.{" "}
-                        </p>
                     </div>
                 </div>
             </div>
